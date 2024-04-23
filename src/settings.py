17a6f9817523a72498aa8f9aaed82b6116dc8bbe
ZemskovIK <igor@zemskoff.su>
2024-04-23 21:15:04 +0300

    Website FastAPI

--- src/settings.py
+++ src/settings.py
@@ -2,7 +2,7 @@ class DataBase:
     USER = 'db_user'
     PASSWORD = 'db_user'
     PORT = '5432'
-    HOST = '172.20.10'
+    HOST = '192.168.88.101'
     NAME = 'maindb'
     SCHEMA = 'main'
     URL = f'postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{NAME}'
